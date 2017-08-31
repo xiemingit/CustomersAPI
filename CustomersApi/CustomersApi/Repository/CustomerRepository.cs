c4f58177b68e914e59ef85c36d29790912e3866f
xiemingit <minxie.net@gmail.com>
2017-08-31 09:48:34 -0700

    Add new users

--- CustomersApi/CustomersApi/Repository/CustomerRepository.cs
+++ CustomersApi/CustomersApi/Repository/CustomerRepository.cs
@@ -14,7 +14,9 @@ namespace CustomersApi.Repository
 			customers.Add(new Customer() { Name = "Nice customer", Address = "USA", Telephone = "123345456" });
 			customers.Add(new Customer() { Name = "Good customer", Address = "UK", Telephone = "9878757654" });
 			customers.Add(new Customer() { Name = "Awesome customer", Address = "France", Telephone = "34546456" });
-			return customers;
+            //Add new user
+            customers.Add(new Customer() { Name = "Min Xie", Address = "USA", Telephone = "88888888" });
+            return customers;
 		}
 	}
 }
